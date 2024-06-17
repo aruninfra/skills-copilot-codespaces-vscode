503baaf75d1bcb528b6964142d1ca103bdaae831
Arun Mohan <43655359+aruninfra@users.noreply.github.com>
2024-06-17 01:36:53 +0000

    Copilot third commit updated

--- comments.js
+++ comments.js
@@ -1,4 +1,11 @@
 // create web server
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+});
 
- 
+server.listen(3000, () => {
+  console.log('Server running at <http://localhost:3000/>');
+});
